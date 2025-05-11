c8aa859417d52ee43233b085ade430a2afa52f86
FerdiGul <0xfrd1gul@gmail.com>
2025-05-11 21:45:45 +0300

    Update VulnWasher.py

--- IoT-HomeDevice/VulnWasher.py
+++ IoT-HomeDevice/VulnWasher.py
@@ -1,5 +1,5 @@
 """
-PoC: Mobile-IoT Control API Token Exchange & Data Access (Account Takeover)
+PoC: Mobile-IoT Control API Token Exchange & Data Access (Account Takeover Vulnerability) - CRITICAL
 Author: Ferdi Gül
 GitHub: https://github.com/FerdiGul
 LinkedIn: https://www.linkedin.com/in/gulferdi/
